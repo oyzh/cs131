be386def325cecdc3d857dddf6cfad7666a3f3d3
oyzh <oyzh@oyzh.oyzh>
2016-03-26 21:52:56 +0800

    PA2

---
+++ PA2/studentFiles/code/NormalizeFeatures.m
@@ -0,0 +1,24 @@
+function featuresNorm = NormalizeFeatures(features)
+% Normalize image features to have zero mean and unit variance. This
+% normalization can cause k-means clustering to perform better.
+%
+% INPUTS
+% features - An array of features for an image. features(i, j, :) is the
+%            feature vector for the pixel img(i, j, :) of the original
+%            image.
+%
+% OUTPUTS
+% featuresNorm - An array of the same shape as features where each feature
+%                has been normalized to have zero mean and unit variance.
+
+    features = double(features);
+    featuresNorm = features;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                              %
+%                                YOUR CODE HERE:                               %
+%                                                                              %
+%                HINT: The functions mean and std may be useful                %
+%                                                                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+end
